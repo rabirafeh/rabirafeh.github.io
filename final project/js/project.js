5921167c8172899b65addee1afdc6e6ce5b2a7ab
rabirafeh <rabirafeh@gmail.com>
2017-06-13 02:09:20 -0400

    commit FP 6.13

---
+++ final project/js/project.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+function goBack() {
+    window.history.back();
+}
+
+	var apple =["Apple", "Manzana", "Pomme"]
+
+	 
+// [
+// 	{
+// 		english: 'apple',
+// 		spanish: 'manzana',
+// 		french: 'pomme',
+// 		image: 'apple.png'
+// 	}, 
+// 	{
+// 		english: 'bear',
+// 		spanish: 'oso',
+// 		french: 'ours',
+// 		image: 'bear.png'
+// 	}
+// ]
+
+// [
+// 	{
+// 		english: 'apple',
+// 		french: 'pomme',
+// 		image: 'apple.png'
+// 	}, 
+// 	{
+// 		english: 'bear',
+// 		french: 'ours',
+// 		image: 'bear.png'
+// 	}
+// ]
+
+
+});
