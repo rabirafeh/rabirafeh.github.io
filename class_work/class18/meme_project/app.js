16c83b2401feeb35eb960ce1e4fb9a3c127bd9d2
rabirafeh <rabirafeh@gmail.com>
2017-06-15 20:42:08 -0400

    class work 6.16

---
+++ class_work/class18/meme_project/app.js
@@ -0,0 +1,14 @@
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5");
+xhr.done(function(data) { 
+
+	// var videoOne = data.data[0].images.looping.mp4;
+
+	// console.log(videoOne);
+
+	data.data.forEach(function(meme){
+		// console.log(meme.images.looping.mp4);
+		var video = meme.images.looping.mp4;
+		$('.gif-list').append("<li><video src='" + video + "' type='video/mp4' autoplay></video></li>")
+	});
+
+});
