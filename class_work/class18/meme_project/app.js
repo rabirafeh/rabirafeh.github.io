6332b912f488cbd3990c044b17d9ddb1bfe717cf
rabirafeh <rabirafeh@gmail.com>
2017-06-15 21:04:26 -0400

    class 6.16 commit

--- class_work/class18/meme_project/app.js
+++ class_work/class18/meme_project/app.js
@@ -1,14 +1,44 @@
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5");
-xhr.done(function(data) { 
+$('.meme-form').submit(function(event){
 
-	// var videoOne = data.data[0].images.looping.mp4;
+	event.preventDefault();
 
-	// console.log(videoOne);
+	$('.gif-list').empty();
 
-	data.data.forEach(function(meme){
-		// console.log(meme.images.looping.mp4);
-		var video = meme.images.looping.mp4;
-		$('.gif-list').append("<li><video src='" + video + "' type='video/mp4' autoplay></video></li>")
+	var memeInputVal = $('.meme-input').val();
+
+	console.log(memeInputVal);
+
+	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + memeInputVal + "&api_key=dc6zaTOxFJmzC&limit=5");
+	xhr.done(function(data) { 
+
+		data.data.forEach(function(meme){
+
+			var video = meme.images.looping.mp4;
+			$('.gif-list').append("<li><video src='" + video + "' type='video/mp4' autoplay></video></li>")
+			})
 	});
+})
 
-});
+
+// $('.meme-form').submit(function(event){
+
+// 	event.preventDefault();
+
+// 	var memeInputVal = $('.meme-input').val();
+
+// 	console.log(memeInputVal);
+
+// 	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5");
+// xhr.done(function(data) { 
+
+// 	// var videoOne = data.data[0].images.looping.mp4;
+
+// 	// console.log(videoOne);
+
+// 	data.data.forEach(function(meme){
+// 		// console.log(meme.images.looping.mp4);
+// 		var video = meme.images.looping.mp4;
+// 		$('.gif-list').append("<li><video src='" + video + "' type='video/mp4' autoplay></video></li>")
+// 		})
+// 	});
+// })
